6c2289d4826a235bfcb4a8172c81141b83f555e5
travinh <vinh.v.tran@ucdenver.edu>
2016-01-24 19:30:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include <iostream>
 
 int main() {
-    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
     std::cout << "Hello, world! My number is " << 105914142%13 << '!' << std::endl;
 
     return 0;
